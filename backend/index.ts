09ea6b29228dff25920165017f02b90699d75ae4
alaa-farhati <farhatiala7@gmail.com>
2023-06-02 11:53:01 +0100

    done server side

---
+++ backend/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import pgPromise from 'pg-promise';
+
+dotenv.config();
+
+const app = express();
+const db = pgPromise()(process.env.DATABASE_URL as string); 
+const port = process.env.PORT 
+
+
+db.connect()
+  .then(() => {
+    console.log('Database connected');
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
+
+app.get('/', (req, res) => {
+  res.send('Hello, 9lewi');
+});
+
