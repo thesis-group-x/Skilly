f7a4ef0b13daf81af476e20898e8edd42bad13c3
alaa-farhati <farhatiala7@gmail.com>
2023-06-18 04:45:23 +0100

    fixed backend

--- client/lib/market/components/payment/points.dart
+++ client/lib/market/components/payment/points.dart
@@ -30,15 +30,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,32 +41,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: ElevatedButton(
           onPressed: () {
             makepayment("100", "INR");
@@ -83,7 +59,6 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Text("Pay"),
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
@@ -91,6 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> makepayment(String amount, String currency) async {
     try {
       paymentIntentData = await createPaymentIntent(amount, currency);
+      print(paymentIntentData);
       if (paymentIntentData != null) {
         await Stripe.instance.initPaymentSheet(
             paymentSheetParameters: SetupPaymentSheetParameters(
@@ -123,6 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 "Bearer sk_test_51NJi2oFuE67mowKSlRn8ryRmofwMCCUNHzSCstl2wYXQJ0lKN5ocL33v1saQtHK3j1Oncv4Bx3h95LT8PZ7UD3ME00HvxiYhq2",
             'Content-Type': 'application/x-www-form-urlencoded'
           });
+      print(response);
       return jsonDecode(response.body);
     } catch (err) {
       print("err charging user $err");
